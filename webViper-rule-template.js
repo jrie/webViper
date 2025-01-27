3f39ab81b40ef36042f95695b44d8de43939f907
Jan Riechers <jan@dwrox.net>
2025-01-27 21:05:19 +0100

    Add rule template
    Add a almost empty rule template for easier configuration.

---
+++ webViper-rule-template.js
@@ -0,0 +1,26 @@
+//
+// Rule template for "webViper" --> https://github.com/jrie/webViper
+// for easier addition, just fill to get started..
+//
+//
+// const ruleSet = {
+//
+    'websiteURL.net': {
+        keywords: [
+            // 'keyword1',
+            // 'keyword2',
+        ],
+        excludes: [
+            // Can be empty
+            // 'string1',
+            // 'string2'
+        ],
+        elementContainers: {
+            //CSSselector1: ['cssSelectorParent1', 'cssSelectorParent2'],
+            //CSSselector2: ['cssSelectorParent1', 'cssSelectorParent2']
+        },
+        removeElement: true
+}
+//
+// }
+//
